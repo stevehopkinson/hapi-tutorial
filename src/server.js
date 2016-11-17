19958b4d68beff80af80f0968f666562610b1f7f
Steve Hopkinson <steve@stevehopkinson.co.uk>
2016-11-17 11:40:55 +0000

    Install Hapi and set up default routes for serving files

--- src/server.js
+++ src/server.js
@@ -5,13 +5,28 @@ const server = new hapi.Server();
 
 server.connection({ port: port });
 
-server.route({
-  method: 'GET',
-  path: '/',
-  handler: (request, response) => response('Hello, world!')
+server.register(require('inert'), (err) => {
+  if (err) throw err;
+  console.log('Inert registered on server');
+
+  server.route({
+    method: 'GET',
+    path: '/{file*}',
+    handler: {
+      directory: {
+        path: 'public'
+      }
+    }
+  })
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, response) => response.file(`./public/index.html`)
+  })
 });
 
 server.start( err => {
   if (err) throw err;
-  console.log(`server is running on : ${server.info.uri}`);
+  console.log(`Server is running on: ${server.info.uri}`);
 });
