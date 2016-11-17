9c4b3d6dd9ad75076adb1eb622ec222012f663b9
Steve Hopkinson <steve@stevehopkinson.co.uk>
2016-11-17 11:09:48 +0000

    Add basic routing to server

--- src/server.js
+++ src/server.js
@@ -1,2 +1,17 @@
 const hapi = require('hapi');
 const port = process.env.PORT || 4000;
+
+const server = new hapi.Server();
+
+server.connection({ port: port });
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: (request, response) => response('Hello, world!')
+});
+
+server.start( err => {
+  if (err) throw err;
+  console.log(`server is running on : ${server.info.uri}`);
+});
